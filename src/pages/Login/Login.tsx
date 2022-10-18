09b940ffda97e08cc6b9b18d6c8f03ad36913984
Asim <asim.thoughtwin@gmail.com>
2022-10-18 16:23:49 +0530

    Add: Redux and Material UI

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,18 @@
+import { RootState } from '@/redux/Store/store'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from '../../redux/Reducer'
 
 export const Login = () => {
-  return <div>Login Page...</div>
+  const count = useSelector((state: RootState) => state.counter.value)
+  console.log({ count })
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div>Login Page...</div>
+      <button aria-label='Increment value' onClick={() => dispatch(increment())}>
+        Click Me !
+      </button>
+    </>
+  )
 }
